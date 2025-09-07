7e56b7361027eac4a2df2fa067170d9c96bfe69d
MaksPzm <makspzm@gmail.com>
2025-09-07 23:45:11 +0500

    Доделал модуль defCity

--- src/Components/definingCity/DefiningCity.tsx
+++ src/Components/definingCity/DefiningCity.tsx
@@ -1,18 +1,20 @@
 import React, { ChangeEvent, RefObject, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useFentchCityWeather } from "../../Api/api";
 import styles from "./definingCity.module.scss"
+import { log } from "console";
 
 interface ComponentProps {
     nameCity: Function
 }
 
 type elRef = RefObject<HTMLInputElement>;
-
+type data = string;
 
 export default function DefiningCity({nameCity}: ComponentProps): React.JSX.Element {
     const [nav, setNav] = useState<{lat: number, lon: number}>({lat: 0, lon: 0});
-    const [city, setCity] = useState<null | string>(null)
-    const [showBlockСhoosingCity, setShowBlockСhoosingCity] = useState<boolean>(true);
+    const [city, setCity] = useState<string>("Москва")
+    const showBlockСhoosingCity: boolean = false;
+    // const [showBlockСhoosingCity, setShowBlockСhoosingCity] = useState<boolean>(false);
     const elRef = useRef<unknown>(null) as elRef;
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => {
@@ -21,26 +23,37 @@ export default function DefiningCity({nameCity}: ComponentProps): React.JSX.Elem
         })
     }, [])
     const data = useFentchCityWeather(nav.lat, nav.lon);
+    const { name } = data;
+    useEffect(() => {
+            if (typeof name !== "string") return;
+            setCity(name)
+    }, [data])
     const clickY = useCallback(() => {
-        nameCity(data.name);
-        setShowBlockСhoosingCity(false)
-    }, [showBlockСhoosingCity])
+        if (typeof name !== "string") return;
+        setCity(name)
+        nameCity(city, showBlockСhoosingCity);
+    }, [city]);
     const clickN = useCallback(() => {
         if (elRef !== null) {
             return elRef.current.focus()
         }
-    }, [])
+    }, []);
     const clickDone = useCallback(()=> {
-        nameCity(city);
-        setShowBlockСhoosingCity(false)
-    }, [showBlockСhoosingCity])
-    console.log("city", city);
+        // setShowBlockСhoosingCity(false);
+        nameCity(city, showBlockСhoosingCity);
+    }, [city, showBlockСhoosingCity])
+    const handlerKeyPress = useCallback((e: React.KeyboardEvent<HTMLInputElement>) => {
+        const { key } = e;
+        if (key === "Enter") {
+            clickDone()
+        }
+    }, [city, showBlockСhoosingCity]);
     
     return (
         <div className={styles.definingCity}>
             <div className={styles.definingCity__geo}>
-                <div className={styles.definingCity__geo_city}>Ваш город: 
-                    <span className={styles.definingCity__geo_city_name}> {data.name}?</span>
+                <div className={styles.definingCity__geo_city}>Показать погоду в н/п: 
+                    <span className={styles.definingCity__geo_city_name}> {name}?</span>
                 </div>
                 <div className={styles.definingCity__geo_blockBtn}>
                     <button className={styles.definingCity__geo_blockBtn_btn} onClick={clickY} type="button">Да</button>
@@ -48,9 +61,12 @@ export default function DefiningCity({nameCity}: ComponentProps): React.JSX.Elem
                 </div>
             </div>
             <div className={styles.definingCity__blockSearch}>
-                <input className={styles.definingCity__blockSearch_input} onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                    setCity(e.target.value)
-                }} ref={elRef} type="text" placeholder="Введите нужный город"/>
+                <input className={styles.definingCity__blockSearch_input}
+                    onKeyDown={handlerKeyPress} 
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                        setCity(e.target.value)
+                    }} ref={elRef} type="text" placeholder="Введите нужный город"
+                />
                 <button className={styles.definingCity__blockSearch_btn} onClick={clickDone} type="button">Готово</button> 
             </div>
            
--- src/Components/layout/Layout.tsx
+++ src/Components/layout/Layout.tsx
@@ -1,16 +1,36 @@
-import React, { useState } from "react"
+import React, { ReactNode, RefObject, useCallback, useEffect, useRef, useState } from "react"
 import styles from "./layout.module.scss"
 import DefiningCity from "../definingCity/DefiningCity"
 
+export type elRefDiv = RefObject<HTMLDivElement>;
+
 export default function Layout(): React.JSX.Element {
+    const blockDefCity = useRef<unknown>(null) as elRefDiv;
+    const blockWrapper = useRef<unknown>(null) as elRefDiv;
     const [city, setCity] = useState<string>("");
-    const nameCity = (name: string) => {
+    const [showDefiningCity, setShowDefiningCity] = useState<boolean>(true);
+    const nameCity = useCallback((name: string, showBlockDefiningCity: boolean) => {
         setCity(name)
-    }
+        setShowDefiningCity(showBlockDefiningCity)
+    }, []);
+    const showBlockDefCity = useCallback(() => {
+
+    }, [showDefiningCity])
+    useEffect(() => {
+        if (!showDefiningCity) {
+            blockDefCity.current.remove()
+        } else {
+            blockWrapper.current.insertAdjacentElement("afterbegin", blockDefCity.current)
+        }
+    }, [showDefiningCity])
     return (
-        <div className={styles.wrapper}>
+        <div className={styles.wrapper} ref={blockWrapper}>
+            <div className={styles.wrapper__defCity} ref={blockDefCity}>
+                {showDefiningCity && <DefiningCity nameCity={nameCity}/>}
+            </div>
             <div className={styles.wrapper__section}>
-                <DefiningCity nameCity={nameCity}/>
+                
+
             </div>
         </div>
     )
